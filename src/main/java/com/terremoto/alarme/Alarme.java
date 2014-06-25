0fe0d04fe5da53a3e58e4c91bb9411b88e9b020f
rodrigovilar <rvilar@gmail.com>
2014-06-25 19:01:28 -0300

    Testes de alerta simples

--- src/main/java/com/terremoto/alarme/Alarme.java
+++ src/main/java/com/terremoto/alarme/Alarme.java
@@ -1,4 +1,34 @@
 package com.terremoto.alarme;
 
 public class Alarme {
+
+	private Exercito exercito;
+	private GuardaCosteira guardaCosteira;
+	private CorpoBombeiros corpoBombeiros;
+
+	public void tremorDetectado(int magnitude, boolean terrestre) {
+		
+		if (terrestre) {
+			exercito.suporteTragedia();
+		} else {
+			guardaCosteira.alertaTsunami();
+		}
+	}
+	
+	public void queimadaDetectada() {
+		corpoBombeiros.alertaIncendio();
+	}
+
+	public void setExercito(Exercito exercito) {
+		this.exercito = exercito;
+	}
+
+	public void setGuardaCosteira(GuardaCosteira guardaCosteira) {
+		this.guardaCosteira = guardaCosteira;
+	}
+
+	public void setCorpoBombeiros(CorpoBombeiros corpoBombeiros) {
+		this.corpoBombeiros = corpoBombeiros;
+	}
+
 }
--- src/test/java/com/terremoto/alarme/AlarmeTest.java
+++ src/test/java/com/terremoto/alarme/AlarmeTest.java
@@ -1,5 +1,59 @@
 package com.terremoto.alarme;
 
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
 
 public class AlarmeTest {
+	
+	
+	@Test
+	public void terremotoForte() {
+		//Criar mocks
+		Exercito exercito = mock(Exercito.class);
+		
+		//Preparar ambiente
+		Alarme alarme = new Alarme();
+		alarme.setExercito(exercito);
+		
+		//Efetuar um estímulo
+		alarme.tremorDetectado(8, true);
+
+		//Verificar se os mocks foram acionados 
+		verify(exercito).suporteTragedia();
+	}
+	
+	@Test
+	public void maremotoForte() {
+		//Criar mocks
+		GuardaCosteira guardaCosteira = mock(GuardaCosteira.class);
+		
+		//Preparar ambiente
+		Alarme alarme = new Alarme();
+		alarme.setGuardaCosteira(guardaCosteira);
+		
+		//Efetuar um estímulo
+		alarme.tremorDetectado(8, false);
+
+		//Verificar se os mocks foram acionados 
+		verify(guardaCosteira).alertaTsunami();
+	}
+	
+	@Test
+	public void queimada() {
+		//Criar mocks
+		CorpoBombeiros corpoBombeiros = mock(CorpoBombeiros.class);
+		
+		//Preparar ambiente
+		Alarme alarme = new Alarme();
+		alarme.setCorpoBombeiros(corpoBombeiros);
+		
+		//Efetuar um estímulo
+		alarme.queimadaDetectada();
+
+		//Verificar se os mocks foram acionados 
+		verify(corpoBombeiros).alertaIncendio();
+	}
+
 }
